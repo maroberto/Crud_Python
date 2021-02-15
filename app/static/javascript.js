a3fdbad72fe852a9eeee3a26e7e45e1168fd828a
maroberto <maroberto13@gmail.com>
2021-02-14 22:36:51 -0300

    Implementação do delete

---
+++ app/static/javascript.js
@@ -0,0 +1,18 @@
+(function(win, doc){
+    'use strict';
+    
+    
+    // verifica se o usuário realemente quer deletar o dado
+    if(doc.querySelector('.btnDel')){
+        let btnDel = doc.querySelectorAll('.btnDel');
+        for(let i=0; i<btnDel.length; i++){
+            btnDel[i].addEventListener('click', function(event){
+                if(confirm('Deseja mesmo apagar este dado?')){
+                    return true;
+                }else{
+                    event.preventDefault();
+                }
+            });
+        }
+    }
+})(window.document);
--- app/views.py
+++ app/views.py
@@ -43,3 +43,9 @@ def update(request, pk):
     if form.is_valid():
         form.save()
         return redirect('home')
+
+
+def delete(request, pk):
+    db = Carros.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
